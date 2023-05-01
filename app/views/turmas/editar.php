a6086840940dd1b3277c63e0278a77873fadddcd
momentoalmir <momentoalmir@gmail.com>
2023-05-01 11:02:26 -0300

    feature: Remove extra html in views

--- app/views/turmas/editar.php
+++ app/views/turmas/editar.php
@@ -1,27 +1,17 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Turma</title>
-</head>
-<body>
-    <h1>Sistema Acadêmico</h1>
-    <p>Editar Turma</p>
 
-    <form action="/turmas" method="put">
-        <p>
-        <label for="descTurma">Descrição da Turma</label>
-        <input type="text" name="descTurma" id="descTurma" value="<?= $context['turma']['descTurma'] ?>">
-        </p>
+<h1>Sistema Acadêmico</h1>
+<p>Editar Turma</p>
 
-        <p>
-        <label for="ano">Ano</label>
-        <input type="text" name="ano" id="ano" value="<?= $context['turma']['ano'] ?>">
-        </p>
+<form action="/turmas" method="put">
+    <p>
+    <label for="descTurma">Descrição da Turma</label>
+    <input type="text" name="descTurma" id="descTurma" value="<?= $context['turma']['descTurma'] ?>">
+    </p>
 
-        <button type="submit">Salvar</button>
-    </form>
-</body>
-</html>
+    <p>
+    <label for="ano">Ano</label>
+    <input type="text" name="ano" id="ano" value="<?= $context['turma']['ano'] ?>">
+    </p>
+
+    <button type="submit">Salvar</button>
+</form>
--- app/views/turmas/novo.php
+++ app/views/turmas/novo.php
@@ -1,27 +1,18 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nova Turma</title>
-</head>
-<body>
-    <h1>Sistema Acadêmico</h1>
-    <p>Nova Turma</p>
 
-    <form action="/turmas" method="post">
-        <p>
-        <label for="descTurma">Descrição da Turma</label>
-        <input type="text" name="descTurma" id="descTurma">
-        </p>
+<h1>Sistema Acadêmico</h1>
+<p>Nova Turma</p>
 
-        <p>
-        <label for="ano">Ano</label>
-        <input type="text" name="ano" id="ano">
-        </p>
+<form action="/turmas" method="post">
+    <p>
+    <label for="descTurma">Descrição da Turma</label>
+    <input type="text" name="descTurma" id="descTurma">
+    </p>
 
-        <button type="submit">Salvar</button>
-    </form>
-</body>
-</html>
+    <p>
+    <label for="ano">Ano</label>
+    <input type="text" name="ano" id="ano">
+    </p>
+
+    <button type="submit">Salvar</button>
+</form>
+
